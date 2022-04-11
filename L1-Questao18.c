76a5427e7dd6e03c659742c22bf43a7f53594bbf
Keven-Lucas <103372443+Keven-Lucas@users.noreply.github.com>
2022-04-11 18:41:12 -0300

    Create L1-Questao18.c

---
+++ L1-Questao18.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+  float raio, diam, val_circ, area;
+  // diam: diametro, val_circ: valor da circunferencia.
+
+  printf("Informe o valor do raio: ");
+  scanf("%f", &raio);
+
+  diam = raio + raio;
+  val_circ = ((2 * (3.14 * (raio * raio))));
+  area = (3.14 * (raio * raio));
+  
+  printf(" Informacoes:\n raio: %.1f\n diametro: %.1f\n valor da circunferencia: %.1f\n area: %.1f\n",raio,diam,val_circ,area);
+  
+  return 0;
+}
