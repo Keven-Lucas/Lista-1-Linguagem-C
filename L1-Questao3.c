7646781aaba1b38162bb0879d2b423c8a9397e81
Keven-Lucas <103372443+Keven-Lucas@users.noreply.github.com>
2022-04-11 18:28:51 -0300

    Create L1-Questao3.c

---
+++ L1-Questao3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+  int num;
+
+  printf("Informe um valor inteiro:");
+  scanf("%d",&num);
+
+  printf("\nHexadecimal: %X\n",num);
+  printf("Octal: %o\n",num);
+  
+  return 0;
+}
